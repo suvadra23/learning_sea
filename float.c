07099f9ff70bb846c463d5cde50d382c15dc03d0
Suvadra Kundu <75944628+suvadra23@users.noreply.github.com>
2023-08-13 11:35:11 +0530

    to display float
    c code to display float numbers

---
+++ float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+float a;
+float b;
+printf("Enter 1st number: ");
+scanf("%f",&a);
+printf("Enter 2nd number: ");
+scanf("%f",&b);
+printf("The numbers entered are %.2f and %.2f  ",a,b);
+return;
+}
