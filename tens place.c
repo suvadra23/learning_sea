6f31785e09300e80474258cdbb35d20e9d1a34b8
Suvadra Kundu <75944628+suvadra23@users.noreply.github.com>
2023-08-15 22:16:54 +0530

    to find the tens place digit of a number
    it has important application in finding dates and leap year,etc etc

---
+++ tens place.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("Enter the number : ");
+    scanf("%d",&a);
+    b=a/10;
+    c=b%10;
+    printf("The 10th place unit is %d",c);
+   
+}
